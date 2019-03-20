22ae9d4b1a3f6086ee6965bed6eeb0662f34c100
jaejoonsong@gmail.com <jaejoonsong@gmail.com>
2019-03-19 22:56:21 -0400

    R package agseDesign

--- R/operatingChar.R
+++ R/operatingChar.R
@@ -1,7 +1,9 @@
 #' @export
 
-operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2,int.u2,f1){
+operatingChar <-function(alpha,information.fraction,th1,th2,f1){
 
+  if( any(f1 < 0 | f1 > 1) ) stop('f1 should be between 0 and 1')
+  if( any(information.fraction < 0 | information.fraction > 1) ) stop('information.fraction should be between 0 and 1')
 
   myresults <- list()
 
@@ -13,6 +15,8 @@ operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2
   au2.Star<-alpha.Star*(1-t)
   al2.Star<-(1-alpha.Star)*(1-t)
 
+  int.u1<-qnorm(au1.Star,mean=0,sd =1,lower.tail=FALSE)
+  int.l1<-qnorm(al1.Star,mean=0,sd =1,lower.tail=TRUE)
 
   Stage1.Limits.Opt <- optim(c(int.l1,int.u1),
                              Stage1.Boundary.Values,
@@ -25,13 +29,12 @@ operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2
                              method="Nelder-Mead",
                              control=list(maxit=1000))
 
-  # For now I am keeping int.l1 and int.u1 in the above function just for checking different values.
-  # Once we decidie which initial value we use, we will replace these two variables by those
-  # values, otherwise giving users option to use different values will create complexity
-
   Limits.1<-cbind(Stage1.Limits.Opt$par[1],Stage1.Limits.Opt$par[2])
   myresults$Stage1.Limits <- as.vector(Limits.1)
   names(myresults$Stage1.Limits) <- c('Lower Limit','Upper Limit')
+
+  int.l2<-Stage1.Limits.Opt$par[2]
+  int.u2<-Stage1.Limits.Opt$par[2]
 
   Stage2.Limits.Opt <- optim(c(int.l2,int.u2),
                              Stage2.Boundary.Values,
@@ -46,11 +49,6 @@ operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2
                              method="Nelder-Mead",
                              control=list(maxit=1000))
 
-  # The same will apply for int.l2 and int.u2
-  # We may also need to make sure these two values are not more than the value of Stage1.Limits.Opt$par[2]
-  # Jae: Can you please think about an option to write the code so that this value does not exceed
-  # the upper limit of the previous stage I upper limit, that is Stage1.Limits.Opt$par[2].
-
   Limits.2<-cbind(Stage2.Limits.Opt$par[1],Stage2.Limits.Opt$par[2])
   myresults$Stage2.Limits <- as.vector(Limits.2)[1]
   names(myresults$Stage2.Limits) <- c('Upper Limit')
@@ -61,10 +59,7 @@ operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2
                   a=Limits.1[1],b=Limits.1[2],
                   th1=c(1,1),th2=c(1,1),
                   optimize=T,method="Nelder-Mead",control=list(maxit=1000))
-  #myresults$D1.Hat <- matrix(D1.Hat[[1]],ncol=2)
 
-  # Computation of estimated stage2 information under th1=(1,1) and th2=(1,1) and D1.Hat
-  # Should we use D1.Hat in the estimation of D2.Hat or use D=c(1,1,1,1)?
   D2.Hat<-optim(c(1,1,1,1),
                 Stage2.Inf.Optimization,
                 lims2=c(Limits.2[1],Limits.2[2]),
@@ -74,7 +69,6 @@ operatingChar <-function(alpha,information.fraction,th1,th2,int.l1,int.u1,int.l2
                 optimize=T,
                 method="Nelder-Mead",
                 control=list(maxit=1000))
-  #myresults$D2.Hat <- matrix(D2.Hat[[1]],ncol=2)
 
   # Computation of total estimated information
   D.Hat<-sum(D1.Hat$par)+sum(D2.Hat$par)
